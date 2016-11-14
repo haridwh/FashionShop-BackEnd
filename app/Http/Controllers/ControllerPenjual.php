bfa22d9c20301a79b204e86aa846cb0f0cbb8e24
dnladi1 <daniel12051996@gmail.com>
2016-11-14 08:02:13 +0700

    tambah controller pembeli penjual

---
+++ app/Http/Controllers/ControllerPenjual.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Penjual;
+
+class ControllerPenjual extends Controller
+{
+    public function RegistrasiPenjual(Request $request){
+    	$Penjual = new Penjual();
+    	$Penjual->id_user = $request->input('id_user');
+    	$Penjual->nip = $request->input('nip');
+    	$Penjual->save();
+
+    return redirect('UIPenjual');//buat diroutes buat ngakses diwebnya nnti
+    }
+
+}
